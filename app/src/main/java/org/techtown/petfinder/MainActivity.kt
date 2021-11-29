4b6e1ffdcb99bc262a0fc0c4b4c601e5cbc76c08
KIMMKyungsik <93589990+KIMMKyungsik@users.noreply.github.com>
2021-11-29 09:37:07 +0900

    파이어베이스 회원가입기능

---
+++ app/src/main/java/org/techtown/petfinder/MainActivity.kt
@@ -0,0 +1,38 @@
+package org.techtown.petfinder
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
+import org.techtown.petfinder.databinding.ActivityMainBinding
+
+
+class MainActivity : BaseActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var auth: FirebaseAuth
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setValues()
+        setupEvents()
+        auth = FirebaseAuth.getInstance()
+    }
+
+    override fun setupEvents() {
+        binding.signUpButton.setOnClickListener() {
+
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+
+        }
+
+
+    }
+
+    override fun setValues() {
+
+    }
+}
---
+++ app/src/main/java/org/techtown/petfinder/SignUpActivity.kt
@@ -0,0 +1,59 @@
+package org.techtown.petfinder
+
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import org.techtown.petfinder.databinding.ActivitySignUpBinding
+
+class SignUpActivity : BaseActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
+    private lateinit var auth: FirebaseAuth
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setValues()
+        setupEvents()
+
+        auth = FirebaseAuth.getInstance()
+        binding.btnRegister.setOnClickListener {
+            createAccount(
+                binding.edtNewEmail.text.toString(),
+                binding.edtNewPassword.text.toString()
+            )
+        }
+    }
+
+    override fun setupEvents() {
+    }
+
+    private fun createAccount(email: String, password: String) {
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "계정 생성 완료.", Toast.LENGTH_SHORT).show()
+                    finish() // 가입창 종료
+                } else {
+                    Toast.makeText(this, "계정 생성 실패", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    override fun setValues() {
+    }
+}
+
+
+
+
+
+
+
+
+
+
