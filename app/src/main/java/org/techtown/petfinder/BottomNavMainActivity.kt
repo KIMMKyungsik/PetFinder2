33e988fc7a22df67903f9cb511e523b04cc25153
KIMMKyungsik <93589990+KIMMKyungsik@users.noreply.github.com>
2021-11-30 15:57:23 +0900

    바텀네비게이션 실패후 주석처리

--- app/src/main/java/org/techtown/petfinder/BottomNavMainActivity.kt
+++ app/src/main/java/org/techtown/petfinder/BottomNavMainActivity.kt
@@ -1,15 +1,20 @@
 package org.techtown.petfinder
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.inflate
+import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import com.google.firebase.auth.FirebaseAuth
-import org.techtown.petfinder.databinding.ActivityMainBinding
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
+import org.techtown.petfinder.databinding.ActivityBottomNavMainBinding
+import java.util.*
 
 class BottomNavMainActivity : BaseActivity() {
 
-    lateinit var binding: ActivityMainBinding
-
+    lateinit var binding: ActivityBottomNavMainBinding
+    val viewList = ArrayList<View>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,8 +22,56 @@ class BottomNavMainActivity : BaseActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_bottom_nav_main)
         setValues()
         setupEvents()
+        viewList.add(layoutInflater.inflate(R.layout.lost_pet_fragment, null))
+        viewList.add(layoutInflater.inflate(R.layout.find_pet_fragment, null))
+        viewList.add(layoutInflater.inflate(R.layout.activity_review, null))
+        binding.viewPager.adapter = pagerAdapter()
 
+//        binding.viewPager.addOnAdapterChangeListener(object :
+//            ViewPager.SimpleOnPageChangeListener() {
+//            override fun onPageSelected(position: Int) {
+//                when (position) {
+//                    0 -> binding.bottomNavigationView.selectedItemId = R.id.lostPet
+//                    1 -> binding.bottomNavigationView.selectedItemId = R.id.findPet
+//                    2 -> binding.bottomNavigationView.selectedItemId = R.id.review
+//
+//                }
+//
+//
+//            }
+//        })
+//        binding.bottomNavigationView.setOnNavigationItemReselectedListener {
+//        when(it.itemId){
+//
+//            R.id.lostPet -> binding.viewPager.setCurrentItem(0)
+//            R.id.findPet -> binding.viewPager.setCurrentItem(1)
+//            R.id.review -> binding.viewPager.setCurrentItem(2)
+//
+//
+//        }
+//            return@setOnNavigationItemReselectedListener
+//
+//
+//
+//        }
     }
+    inner class pagerAdapter : PagerAdapter() {
+        override fun getCount() = viewList.size
+
+        override fun isViewFromObject(view: View, `object`: Any) = view == `object`
+
+        override fun instantiateItem(container: ViewGroup, position: Int): Any {
+            var currentView = viewList[position]
+            binding.viewPager.addView(currentView)
+            return currentView
+
+        }
+
+        override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+            binding.viewPager.removeView(`object` as View)
+        }
+    }
+
 
     override fun setupEvents() {
 
@@ -28,3 +81,5 @@ class BottomNavMainActivity : BaseActivity() {
 
     }
 }
+
+
