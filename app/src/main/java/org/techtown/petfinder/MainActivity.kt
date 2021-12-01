57af4cb188f560610da727f32cba90f3f7486b39
KIMMKyungsik <93589990+KIMMKyungsik@users.noreply.github.com>
2021-12-02 04:20:34 +0900

    mainactivity 내용물 -> signinactivity로 옮김 (로그인 기능 , 아직코드에러)

--- app/src/main/java/org/techtown/petfinder/MainActivity.kt
+++ app/src/main/java/org/techtown/petfinder/MainActivity.kt
@@ -10,7 +10,7 @@ import org.techtown.petfinder.databinding.ActivityMainBinding
 class MainActivity : BaseActivity() {
 
     lateinit var binding: ActivityMainBinding
-    lateinit var auth: FirebaseAuth
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,22 +18,10 @@ class MainActivity : BaseActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setValues()
         setupEvents()
-        auth = FirebaseAuth.getInstance()
+
     }
 
     override fun setupEvents() {
-        binding.signUpButton.setOnClickListener() {
-
-            val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
-
-        }
-
-        binding.signInButton.setOnClickListener {
-            val intent = Intent(this, ReviewActivity::class.java)
-            startActivity(intent)
-
-        }
 
     }
 
---
+++ app/src/main/java/org/techtown/petfinder/SignInActivity.kt
@@ -0,0 +1,83 @@
+package org.techtown.petfinder
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
+import org.techtown.petfinder.databinding.ActivitySignInBinding
+
+
+class SignInActivity : BaseActivity() {
+
+    lateinit var binding: ActivitySignInBinding
+    lateinit var auth: FirebaseAuth
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        auth = FirebaseAuth.getInstance()
+
+        if (auth.currentUser != null) {
+            val intent = Intent(this, MainActivity::class.java).apply {
+                flags = flags or Intent.FLAG_ACTIVITY_NO_ANIMATION
+            }
+            startActivity(intent)
+            finish()
+            overridePendingTransition(0, 0)
+            return
+        }
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_in)
+
+        setValues()
+        setupEvents()
+    }
+
+    override fun setupEvents() {
+        binding.signUpButton.setOnClickListener() {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.signInButton.setOnClickListener {
+            signIn()
+        }
+    }
+
+    private fun signIn() {
+        val email = binding.emailTextInput.editText!!.text.toString()
+        val password = binding.passwordTextInput.editText!!.text.toString()
+
+        if (email.isBlank()) {
+            binding.emailTextInput.error = "이메일 주소를 입력해 주세요."
+
+        } else {
+            binding.emailTextInput.error = null
+            binding.emailTextInput.isErrorEnabled = false
+        }
+
+        if (password.isBlank()) {
+            binding.passwordTextInput.error = "패스워드를 입력해 주세요."
+
+        } else {
+            binding.passwordTextInput.error = null
+            binding.passwordTextInput.isErrorEnabled = false
+        }
+
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+            it.exception?.let {
+                it.printStackTrace()
+                // 계정이 없거나, 패스워드가 틀림
+
+            } ?: run {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+    override fun setValues() {
+
+    }
+}
